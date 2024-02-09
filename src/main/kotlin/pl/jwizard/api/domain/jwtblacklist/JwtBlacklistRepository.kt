cc361dd173830c6ab72c67882138daa3bfcb3676
Milosz08 <gilgamilosz451@gmail.com>
2024-02-09 05:01:22 +0100

    add missing repository annotation

--- src/main/kotlin/pl/jwizard/api/domain/jwtblacklist/JwtBlacklistRepository.kt
+++ src/main/kotlin/pl/jwizard/api/domain/jwtblacklist/JwtBlacklistRepository.kt
@@ -6,8 +6,10 @@ package pl.jwizard.api.domain.jwtblacklist
 
 import org.bson.types.ObjectId
 import org.springframework.data.mongodb.repository.MongoRepository
+import org.springframework.stereotype.Repository
 import java.time.LocalDateTime
 
+@Repository
 interface JwtBlacklistRepository : MongoRepository<JwtBlacklistDocument, ObjectId> {
 	fun existsByJwt(jwt: String): Boolean
 	fun deleteAllByExpiredAtBefore(now: LocalDateTime)
