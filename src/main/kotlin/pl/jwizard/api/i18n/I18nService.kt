f76c2dd441962aa06aa543de3f8da65225f96033
Milosz08 <gilgamilosz451@gmail.com>
2024-02-09 05:10:21 +0100

    rewrite i18n methods for include locale from outer parameter

--- src/main/kotlin/pl/jwizard/api/i18n/I18nService.kt
+++ src/main/kotlin/pl/jwizard/api/i18n/I18nService.kt
@@ -7,19 +7,20 @@ package pl.jwizard.api.i18n
 import org.springframework.context.NoSuchMessageException
 import org.springframework.context.i18n.LocaleContextHolder
 import org.springframework.stereotype.Service
+import java.util.*
 
 @Service
 class I18nService(
 	private val i18nConfiguration: I18nConfiguration
 ) {
-	fun getMessage(placeholder: String?, params: Map<String, Any>): String {
+	fun getMessage(placeholder: String?, params: Map<String, Any>, locale: Locale): String {
 		val messageSource = i18nConfiguration.messageSource()
 		var text: String
 		try {
 			if (placeholder == null) {
 				return ""
 			}
-			text = messageSource.getMessage(placeholder, null, LocaleContextHolder.getLocale())
+			text = messageSource.getMessage(placeholder, null, locale)
 			if (text.isBlank()) {
 				return text
 			}
@@ -32,12 +33,15 @@ class I18nService(
 		}
 	}
 
-	fun getMessage(placeholder: String): String = getMessage(placeholder, emptyMap())
+	fun getMessage(placeholder: String): String = getMessage(placeholder, emptyMap(), LocaleContextHolder.getLocale())
 
-	fun getMessage(placeholder: LocaleSet): String = getMessage(placeholder.getPlaceholder(), emptyMap())
+	fun getMessage(placeholder: LocaleSet): String =
+		getMessage(placeholder.getPlaceholder(), emptyMap(), LocaleContextHolder.getLocale())
 
 	fun getMessage(
 		placeholder: LocaleSet,
 		variables: Map<String, Any>
-	): String = getMessage(placeholder.getPlaceholder(), variables)
+	): String = getMessage(placeholder.getPlaceholder(), variables, LocaleContextHolder.getLocale())
+
+	fun getMessage(placeholder: String, locale: Locale): String = getMessage(placeholder, emptyMap(), locale)
 }
