5b6f80e686a0476d8cf56702ec86cd107b6db7ee
Milosz08 <gilgamilosz451@gmail.com>
2024-02-11 14:39:28 +0100

    add stateles user details service and base message dto

---
+++ src/main/kotlin/pl/jwizard/api/network/BaseMessagesResDto.kt
@@ -0,0 +1,9 @@
+/*
+ * Copyright (c) 2024 by JWizard
+ * Originally developed by Miłosz Gilga <https://miloszgilga.pl>
+ */
+package pl.jwizard.api.network
+
+data class BaseMessagesResDto(
+	val message: String
+)
