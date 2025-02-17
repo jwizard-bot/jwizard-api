db8ffa9d593953dfcd5b36fb474aa45e04803aa4
Miłosz Gilga <gilgamilosz451@gmail.com>
2025-02-18 00:04:54 +0100

    (feat) remove VcsConfig dependent-bean and httpClientFacade from jwizard library

--- jwa-service/src/main/kotlin/pl/jwizard/jwa/service/RepositoryServiceImpl.kt
+++ jwa-service/src/main/kotlin/pl/jwizard/jwa/service/RepositoryServiceImpl.kt
@@ -1,40 +1,39 @@
 package pl.jwizard.jwa.service
 
+import com.fasterxml.jackson.databind.ObjectMapper
+import org.springframework.stereotype.Component
 import pl.jwizard.jwa.core.cache.CacheEntity
-import pl.jwizard.jwa.core.cache.CacheFacadeBean
-import pl.jwizard.jwa.core.property.EnvironmentBean
+import pl.jwizard.jwa.core.cache.CacheFacade
+import pl.jwizard.jwa.core.i18n.I18nAppFragmentSource
 import pl.jwizard.jwa.core.property.ServerProperty
+import pl.jwizard.jwa.rest.route.repository.RepositoryService
 import pl.jwizard.jwa.rest.route.repository.dto.LastUpdateDto
 import pl.jwizard.jwa.rest.route.repository.dto.PrimaryLanguageDto
 import pl.jwizard.jwa.rest.route.repository.dto.RepositoryResDto
-import pl.jwizard.jwa.rest.route.repository.spi.RepositoryService
 import pl.jwizard.jwa.service.spi.BuildInfoSupplier
 import pl.jwizard.jwa.service.spi.dto.ProjectVersionRow
-import pl.jwizard.jwl.http.HttpClientFacadeBean
-import pl.jwizard.jwl.i18n.I18nBean
-import pl.jwizard.jwl.i18n.source.I18nLibDynamicSource
-import pl.jwizard.jwl.ioc.stereotype.SingletonService
+import pl.jwizard.jwl.i18n.I18n
 import pl.jwizard.jwl.property.AppBaseProperty
+import pl.jwizard.jwl.property.BaseEnvironment
+import pl.jwizard.jwl.property.VcsProperty
 import pl.jwizard.jwl.util.ext.getAsText
-import pl.jwizard.jwl.vcs.VcsConfigBean
-import pl.jwizard.jwl.vcs.VcsRepository
+import java.net.URI
+import java.net.http.HttpClient
+import java.net.http.HttpRequest
+import java.net.http.HttpResponse
 
-@SingletonService
-internal class RepositoryServiceBean(
-	private val vcsConfig: VcsConfigBean,
-	private val cacheFacade: CacheFacadeBean,
-	private val i18n: I18nBean,
-	private val httpClientFacade: HttpClientFacadeBean,
-	private val environment: EnvironmentBean,
+@Component
+internal class RepositoryServiceImpl(
+	private val cacheFacade: CacheFacade,
+	private val i18n: I18n,
+	private val httpClient: HttpClient,
+	private val objectMapper: ObjectMapper,
+	private val environment: BaseEnvironment,
 	private val buildInfoSupplier: BuildInfoSupplier,
 ) : RepositoryService {
-
-	private val githubApiUrl = environment
-		.getProperty<String>(ServerProperty.GITHUB_API_URL)
-
+	private val githubApiUrl = environment.getProperty<String>(ServerProperty.GITHUB_API_URL)
 	private val organizationName = environment
 		.getProperty<String>(AppBaseProperty.VCS_ORGANIZATION_NAME)
-
 	private val githubLanguageColorApiUrl = environment
 		.getProperty<String>(ServerProperty.GITHUB_LANGUAGE_COLOR_API_URL)
 
@@ -64,10 +63,24 @@ internal class RepositoryServiceBean(
 		projectsBuildInfo: Map<String, ProjectVersionRow>,
 		language: String?,
 	): List<RepositoryResDto> {
-		val declaredRepos = VcsRepository.entries.map { environment.getProperty<String>(it.property) }
-		val repositories = httpClientFacade
-			.getJsonListCall("$githubApiUrl/orgs/$organizationName/repos")
-		val colors = httpClientFacade.getJsonListCall(githubLanguageColorApiUrl)
+		val declaredRepos = VcsProperty.entries.map { environment.getProperty<String>(it) }
+
+		val reposHttpRequest = HttpRequest.newBuilder()
+			.uri(URI.create("$githubApiUrl/orgs/$organizationName/repos"))
+			.build()
+
+		val colorsHttpRequest = HttpRequest.newBuilder()
+			.uri(URI.create(githubLanguageColorApiUrl))
+			.build()
+
+		val reposResponse = httpClient.send(reposHttpRequest, HttpResponse.BodyHandlers.ofString())
+		val colorsResponse = httpClient.send(colorsHttpRequest, HttpResponse.BodyHandlers.ofString())
+
+		if (reposResponse.statusCode() != 200 || colorsResponse.statusCode() != 200) {
+			return emptyList()
+		}
+		val repositories = objectMapper.readTree(reposResponse.body())
+		val colors = objectMapper.readTree(colorsResponse.body())
 
 		val declaredRepositories = repositories.filter {
 			val name = it.getAsText("name")
@@ -80,13 +93,13 @@ internal class RepositoryServiceBean(
 			if (latestVersionLong == null || lastUpdatedUtc == null) {
 				continue
 			}
-			val (shortSha, link) = vcsConfig.createSafeSnapshotUrl(name, latestVersionLong)
 			val programmingLanguage = repo.getAsText("language")
 			val color = if (colors.has(programmingLanguage)) {
 				colors.get(programmingLanguage).getAsText("color")
 			} else {
 				null
 			}
+			val repositoryUrl = repo.getAsText("html_url")
 			val repositoryRes = RepositoryResDto(
 				name = i18n.tRaw(I18nAppFragmentSource.PROJECT_NAME, arrayOf(name), language),
 				slug = name,
@@ -113,10 +126,12 @@ internal class RepositoryServiceBean(
 	) = persistedBuildInfo.all {
 		val buildInfo = fetchedBuildInfo[it.slug] ?: return false
 		val shortSha = if (buildInfo.latestVersionLong != null) {
-			vcsConfig.createShortSha(buildInfo.latestVersionLong)
+			createShortSha(buildInfo.latestVersionLong)
 		} else {
 			null
 		}
 		shortSha == it.lastUpdate.buildSha
 	}
+
+	private fun createShortSha(versionLong: String) = versionLong.substring(0, 7)
 }
