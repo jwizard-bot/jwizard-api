289e22d02a5cd78f2b0f133fbedd1d16aca20a6a
Miłosz Gilga <gilgamilosz451@gmail.com>
2025-02-23 00:55:20 +0100

    (fix) fix ProcessDomainDefinition unit test

--- jwa-service/src/test/kotlin/pl/jwizard/jwa/service/domain/ProcessDomainDefinitionTest.kt
+++ jwa-service/src/test/kotlin/pl/jwizard/jwa/service/domain/ProcessDomainDefinitionTest.kt
@@ -3,6 +3,7 @@ package pl.jwizard.jwa.service.domain
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 import pl.jwizard.jwa.service.instance.domain.DevProcessDomainDefinition
+import pl.jwizard.jwa.service.instance.domain.ProcessDefinition
 import pl.jwizard.jwa.service.instance.domain.ProdProcessDomainDefinition
 import pl.jwizard.jwl.vault.kvgroup.VaultKvGroupProperties
 import java.util.*
@@ -21,8 +22,9 @@ class ProcessDomainDefinitionTest {
 
 		val properties = Properties()
 		properties.setProperty("V_SERVER_PORT", randomPort.toString())
+		properties.setProperty("V_SHARDS_PER_PROCESS", "2")
 
-		val expectedPaths = listOf(devPathDefinition.format(randomPort))
+		val expectedPaths = listOf(ProcessDefinition(devPathDefinition.format(randomPort), 2))
 
 		val paths = devProcessDomainDefinition.generatePaths(
 			pathDefinition = devPathDefinition,
@@ -41,9 +43,9 @@ class ProcessDomainDefinitionTest {
 		properties.setProperty("V_SHARD_OVERALL_MAX", "29")
 
 		val expectedPaths = listOf(
-			prodPathDefinition.format(prefix, 0, 11),
-			prodPathDefinition.format(prefix, 12, 23),
-			prodPathDefinition.format(prefix, 24, 29),
+			ProcessDefinition(prodPathDefinition.format(prefix, 0, 11), 11),
+			ProcessDefinition(prodPathDefinition.format(prefix, 12, 23), 11),
+			ProcessDefinition(prodPathDefinition.format(prefix, 24, 29), 5),
 		)
 		val paths = prodProcessDomainDefinition.generatePaths(
 			pathDefinition = prodPathDefinition,
