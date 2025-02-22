3750231d1264e4a1f0b3adc04d8a51f88ecb65cf
Miłosz Gilga <gilgamilosz451@gmail.com>
2025-02-22 23:48:53 +0100

    (feat) remove status from i18n

--- jwa-core/src/main/kotlin/pl/jwizard/jwa/core/i18n/I18nUtilLocaleSource.kt
+++ jwa-core/src/main/kotlin/pl/jwizard/jwa/core/i18n/I18nUtilLocaleSource.kt
@@ -3,9 +3,6 @@ package pl.jwizard.jwa.core.i18n
 import pl.jwizard.jwl.i18n.I18nLocaleSource
 
 enum class I18nUtilLocaleSource(override val placeholder: String) : I18nLocaleSource {
-	STATUS_OPERATIONAL("jwa.util.status.operational"),
-	STATUS_DOWN("jwa.util.status.down"),
-	STATUS_UNKNOWN("jwa.util.status.unknown"),
 	ALL("jwa.util.all"),
 	;
 }
