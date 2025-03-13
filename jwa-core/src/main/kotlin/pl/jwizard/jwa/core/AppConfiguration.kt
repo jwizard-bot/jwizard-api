66a04982e9fadf19e3182a5f896581f3a0158c1a
Miłosz Gilga <gilgamilosz451@gmail.com>
2025-03-13 11:33:37 +0100

    (feat) init yauaa library (user-agent parser)

--- jwa-core/src/main/kotlin/pl/jwizard/jwa/core/AppConfiguration.kt
+++ jwa-core/src/main/kotlin/pl/jwizard/jwa/core/AppConfiguration.kt
@@ -4,16 +4,18 @@ import com.fasterxml.jackson.databind.ObjectMapper
 import com.fasterxml.jackson.databind.SerializationFeature
 import com.fasterxml.jackson.databind.util.StdDateFormat
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
+import nl.basjes.parse.useragent.UserAgent
+import nl.basjes.parse.useragent.UserAgentAnalyzer
 import org.springframework.context.MessageSource
 import org.springframework.context.annotation.Bean
 import org.springframework.stereotype.Component
+import pl.jwizard.jwa.core.property.ServerProperty
 import pl.jwizard.jwl.i18n.I18n
 import pl.jwizard.jwl.i18n.I18nInitializer
 import pl.jwizard.jwl.ioc.IoCKtContextFactory
 import pl.jwizard.jwl.property.BaseEnvironment
 import pl.jwizard.jwl.server.HttpServer
 import pl.jwizard.jwl.server.exception.UnspecifiedExceptionAdvisor
-import pl.jwizard.jwl.server.filter.LanguageHeaderExtractorFilter
 import java.net.http.HttpClient
 
 @Component
@@ -58,7 +60,10 @@ internal class AppConfiguration {
 	fun unspecifiedExceptionAdvisor() = UnspecifiedExceptionAdvisor()
 
 	@Bean
-	fun languageHeaderExtractorFilter(
+	fun userAgentAnalyzer(
 		environment: BaseEnvironment,
-	) = LanguageHeaderExtractorFilter(environment)
+	): UserAgentAnalyzer = UserAgentAnalyzer.newBuilder()
+		.withCache(environment.getProperty(ServerProperty.YAUAA_CACHE_MAX_ELEMENTS))
+		.withFields(UserAgent.OPERATING_SYSTEM_NAME, UserAgent.DEVICE_CLASS)
+		.build()
 }
